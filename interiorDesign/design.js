90f20db15c88eb8b9788d610f273f9b4da850bf8
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2022-01-11 10:08:22 -0800

    navigation & section

---
+++ interiorDesign/design.js
@@ -0,0 +1,9 @@
+// ბურგერბარის ჯავასკრიპტი
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
+  }
+}
